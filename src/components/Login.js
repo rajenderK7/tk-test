f595085ff8b26c92f071c87e68e81f02da0c9ef5
ritish <sritish.10@gmail.com>
2023-05-01 12:40:36 +0530

    conflict 2

--- src/components/Login.js
+++ src/components/Login.js
@@ -3,7 +3,8 @@ import React from "react";
 function Login() {
   return (
     <div>
-      <h1>Login page Rajender is editing this.</h1>
+          <h1>Login page Rajender is editing this.</h1>
+          <h1>Ritish conflict</h1>
     </div>
   );
 }
