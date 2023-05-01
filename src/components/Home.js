08a853cf5f669a2bf7d661d06f8b36fe18588942
Rajender <rajenderkatkuri123@gmail.com>
2023-05-01 12:23:52 +0530

    feat: Add Home page

---
+++ src/components/Home.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Home = () => {
+  return (
+    <div>
+      <h1>Krithomedh is conducting a workshop on Git & GitHub.</h1>
+    </div>
+  );
+};
+
+export default Home;
